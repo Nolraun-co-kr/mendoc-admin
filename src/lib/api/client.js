0f6e3b3d3287bd6f77d534e4d91cf8bec1250933
eonsang <2eonhworking@gmail.com>
2021-05-18 02:16:42 +0900

    feat: chagne base url

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const client = axios.create();
 
 client.defaults.baseURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v1' : 'http://54.180.144.82/api'
+  process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/v1' : 'http://mendoc-stage.ap-northeast-2.elasticbeanstalk.com/api'
 
 client.interceptors.request.use(
   async function (config) {
